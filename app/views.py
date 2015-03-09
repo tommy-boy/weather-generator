38e37a844eb8d190df29f1a019eb62c169e91160
Tom Reese <tgreese@gannett.com>
2015-03-09 13:07:54 -0700

    app.route

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from flask import render_template, request
 from models import Weather
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], endpoint='/')
+@app.route('/index', methods=['GET'])
 def index():
 	forecast = Weather  # get an instance of the class
 	forecast.getForecast()
