ea2625c010318a3125bd0f247f45dc061633aae1
Tom Reese <tgreese@gannett.com>
2015-03-09 13:51:04 -0700

    modify route

--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@ from flask import render_template, request
 from models import Weather
 
 
-@app.route('/', methods=['GET'], endpoint='/')
 @app.route('/index', methods=['GET'])
 def index():
 	forecast = Weather  # get an instance of the class
