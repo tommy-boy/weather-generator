9a92d8703744413f7eca539839faa8881c75c0c4
Tom Reese <tgreese@gannett.com>
2015-03-09 11:35:10 -0700

    refactor class object

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template
 import requests
 import json
 import re
@@ -9,52 +9,60 @@ from datetime import datetime
 
 class Weather(object):
 
-    @classmethod
-    def getForecast(self):
+	@classmethod
+	def getForecast(self):
 	    article_json = app.config['ARTICLE_JSON']
 	    data = requests.get(str(article_json))
 	    if data.status_code == 200:
-	    	narrative = ''
-	    	pos = 1
+	    	content = ''
+	    	offset = 1
 	    	json_obj = data.json['article']['body']
 	    	for cnt in range(0, len(json_obj)):
-	    		if 'type' in json_obj[cnt] and pos <=2:
-	    			narrative += json_obj[cnt]['value'] + '  '
-	    			pos += 1
+	    		if 'type' in json_obj[cnt] and offset <=2:
+	    			content += json_obj[cnt]['value'] + '  '
+	    			offset += 1
 	        cleanr =re.compile('<.*?>')
-	        narrative = re.sub(cleanr,'', narrative)
-	        return text_truncate(narrative)
+	        self.narrative = text_truncate(re.sub(cleanr,'', content))
+	        return
 	    else:
 	        return False
 
-    @classmethod
-    def getCurrent(self):
+	@classmethod
+	def getCurrent(self):
 		config_url = app.config['WEATHER_JSON']
 		data = requests.get(str(config_url))
 		if data.status_code == 200:
-			threeday = OrderedDict()
-			weathericons = OrderedDict()
-			hilo = OrderedDict()
-			json_obj = data.json['primary_modules']
-			for cnt in range(0, len(json_obj)):
-				if 'weather_seven_day' in json_obj[cnt]:
-					hi_to_replace = json_obj[cnt]['weather_seven_day'][0]['tempFHi']
-					lo_to_replace =  json_obj[cnt]['weather_seven_day'][0]['tempFLo']
-					weather_icon = app.config['WEATHER_ICONS'] + str(json_obj[cnt]['weather_seven_day'][0]['dayTime']['weatherIcon']) + '.png'
-			for cnt in range(0, len(json_obj)):
-				if 'weather_seven_day' in json_obj[cnt]:
-					for offset in range(1, 4):
-						dayname = json_obj[cnt]['weather_seven_day'][offset]['dayCode']
-						hi = json_obj[cnt]['weather_seven_day'][offset]['tempFHi']
-						lo =  json_obj[cnt]['weather_seven_day'][offset]['tempFLo']
-						weather_icon = app.config['WEATHER_ICONS'] + str(json_obj[cnt]['weather_seven_day'][offset]['dayTime']['weatherIcon']) + '.png'
-						threeday['day' + str(offset)] = dayname[:3]
-						hilo['day' + str(offset) + '_hi'] = hi
-						hilo['day' + str(offset) + '_lo'] = lo
-						weathericons['icon' + str(offset)] = weather_icon
-			return (hi_to_replace, lo_to_replace, weather_icon, threeday, weathericons, hilo)
+			self.threeday = OrderedDict()
+			self.hilo = OrderedDict()
+			self.weathericons = OrderedDict()
+			self.json_obj = data.json['primary_modules']
+			self.getOneDay(self)
+			self.getThreeDay(self)
+			return
 		else:
 			return False
+
+	@staticmethod
+	def getOneDay(self):
+		for cnt in range(0, len(self.json_obj)):
+			if 'weather_seven_day' in self.json_obj[cnt]:
+				self.hi_to_replace = self.json_obj[cnt]['weather_seven_day'][0]['tempFHi']
+				self.lo_to_replace =  self.json_obj[cnt]['weather_seven_day'][0]['tempFLo']
+				self.weather_icon = app.config['WEATHER_ICONS'] + str(self.json_obj[cnt]['weather_seven_day'][0]['dayTime']['weatherIcon']) + '.png'
+
+	@staticmethod
+	def getThreeDay(self):
+		for cnt in range(0, len(self.json_obj)):
+			if 'weather_seven_day' in self.json_obj[cnt]:
+				for offset in range(1, 4):
+					dayname = self.json_obj[cnt]['weather_seven_day'][offset]['dayCode']
+					hi = self.json_obj[cnt]['weather_seven_day'][offset]['tempFHi']
+					lo =  self.json_obj[cnt]['weather_seven_day'][offset]['tempFLo']
+					weather_icon = app.config['WEATHER_ICONS'] + str(self.json_obj[cnt]['weather_seven_day'][offset]['dayTime']['weatherIcon']) + '.png'
+					self.threeday['day' + str(offset)] = dayname[:3]
+					self.hilo['day' + str(offset) + '_hi'] = hi
+					self.hilo['day' + str(offset) + '_lo'] = lo
+					self.weathericons['icon' + str(offset)] = weather_icon
 
 
 @app.context_processor
@@ -80,4 +88,4 @@ def weather_url():
 	return dict(weather_url=app.config['WEATHER_URL'])
 
 def text_truncate(content, length=120, suffix='...'):
-		return content[:length].rsplit(' ', 1)[0]+suffix
+	return content[:length].rsplit(' ', 1)[0]+suffix
--- app/views.py
+++ app/views.py
@@ -5,9 +5,9 @@ from models import Weather
 
 @app.route('/', methods=['GET'])
 def index():
-	gen = Weather  # get an instance of the class
-	narrative = gen.getForecast()
-	today_hi, today_lo, weather_icon, threeday, weathericons, hilo = gen.getCurrent()
+	forecast = Weather  # get an instance of the class
+	forecast.getForecast()
+	forecast.getCurrent()
 	weatherimg = app.config['IMGDIR'] + app.config['WEATHERIMG']
-	return render_template('index.html', title='Index', narrative=narrative, weatherimg=weatherimg, today_hi=today_hi, today_lo=today_lo, weather_icon=weather_icon, threeday=threeday, weathericons=weathericons, hilo=hilo)
+	return render_template('index.html', title='Index', weatherimg=weatherimg, forecast=forecast)
 
