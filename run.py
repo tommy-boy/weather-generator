a5196b2dfb9a874c71d3ef5c80e85e530e114c50
Tom Reese <tgreese@gannett.com>
2015-03-10 09:37:38 -0700

    change port

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 from app import app
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port = None, debug = None)
-  # app.run(debug = app.config['DEBUG'])
+	port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port, debug = app.config['DEBUG'])
