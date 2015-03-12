0a68daae47fb310f767d9843c869769f44d4a385
Tom Reese <tgreese@gannett.com>
2015-03-12 13:51:16 -0700

    weather front html

--- front.py
+++ front.py
@@ -1,14 +1,23 @@
 import os
 from app import app
-from flask import render_template
+from flask import Flask, request, render_template, Response
+from werkzeug import secure_filename
 from app.models import Weather
 
 
-@app.route('/front')
-def front():
-	front = Weather  # get an instance of the class
-	front.getFront()
-	return render_template('front.html', front=front)
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+@app.route('/front', methods=['GET'])
+def metrics():  # pragma: no cover
+    front = Weather  # get an instance of the class
+    front.getFront()
+    front_file = app.config['UPLOAD_FOLDER'] + "weather_front.html"
+    output_from_parsed_template = render_template('front.html', front=front)
+    with open(front_file, "wb") as f:
+        f.write(output_from_parsed_template)
+        return output_from_parsed_template
+
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
