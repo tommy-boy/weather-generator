66d06ae63412c34ee925271303ba99d4245d29a7
Tom Reese <tgreese@gannett.com>
2015-03-16 14:43:18 -0700

    revert port local

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from app import app
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 33507))
-	app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
-	# app.run(debug=app.config['DEBUG'])
+	# port = int(os.environ.get("PORT", 33507))
+	# app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
+	app.run(debug=app.config['DEBUG'])
 
